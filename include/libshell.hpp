6749ec3fdb31213999170ce7eac14224d71ae8d6
netoe1 <elynetobr@outlook.com>
2023-10-10 08:45:41 -0300

    making

--- include/libshell.hpp
+++ include/libshell.hpp
@@ -4,9 +4,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string>
-#include <vector>
-#include <stdexcept>
-
+#include <list>
 
 // Here we have some constants that is by default in the most OS's.
 
@@ -14,27 +12,53 @@
 #define MKDIR "mkdir "
 #define ECHO "echo "
 
-namespace Functions {
-    class ShellFunctions {
+namespace FileFormats
+{
+    enum FILES_SUPPORTED
+    {
+        TXT_FILE,
+        XML_FILE
+    };
+
+    enum SEARCH_FILTERS
+    {
+        ALL_UPPER,
+        ALL_LOWER,
+        JUST_FIND
+    };
+    class Text
+    {
+    private:
+        std::list<std::string> list;
+
     public:
-        static void createFolders(const std::vector<std::string>& folders);
-        static void removeFolders(const std::vector<std::string>& folders);
-        static void createFolder(const std::string& folder);
-        static void removeFolder(const std::string& folder);
+        Text(std::string data);
+        void addContent(std::string &data);
+        void deleteFile(std::string &namefile);
+        void createFile(int &FILE);
+        void findAWord(std::string &word, short int &OPERATION);
     };
 }
-
-
+namespace Functions
+{
+    class ShellFunctions
+    {
+    public:
+        static void createFolders(const std::vector<std::string> &folders);
+        static void removeFolders(const std::vector<std::string> &folders);
+        static void createFolder(const std::string &folder);
+        static void removeFolder(const std::string &folder);
+    };
+}
 
 #if __linux__
 // Here, we have to make some modifications...
 #define CLEAR "clear"
-#define LS  "ls"
+#define LS "ls"
 
 #elif _WIN32
 #define CLEAR "clear"
-#define LS  "ls"
-
+#define LS "ls"
 
 #endif
 
--- src/libshell.cpp
+++ src/libshell.cpp
@@ -3,47 +3,90 @@
 #include <string>
 #include <vector>
 #include <stdexcept>
+#include <list>
+#include <algorithm>
 
-
-//Custom libraries
+// Custom libraries
 #include "../include/libshell.hpp"
-using namespace Functions;
 
-void ShellFunctions::createFolders(const std::vector<std::string>& folders) {
-    if (folders.empty()) {
+void Functions::ShellFunctions::createFolders(const std::vector<std::string> &folders)
+{
+    if (folders.empty())
+    {
         throw std::runtime_error("You didn't set a valid string for creating folders.\nTry reading the docs if you have any questions.");
     }
 
     std::string parse;
-    for (const std::string& folder : folders) {
+    for (const std::string &folder : folders)
+    {
         parse += folder + " ";
     }
-    
-    std::system((MKDIR+ parse).c_str());
+
+    std::system((MKDIR + parse).c_str());
 }
-void ShellFunctions::removeFolders(const std::vector<std::string>& folders) {
-    if (folders.empty()) {
+void Functions::ShellFunctions::removeFolders(const std::vector<std::string> &folders)
+{
+    if (folders.empty())
+    {
         throw std::runtime_error("You didn't set a valid folder name.");
     }
 
     std::string parse;
-    for (const std::string& folder : folders) {
+    for (const std::string &folder : folders)
+    {
         parse += folder + " ";
     }
 
     std::system((RMDIR + parse).c_str());
 }
-void ShellFunctions::createFolder(const std::string& folder) {
-    if (folder.empty()) {
+void Functions::ShellFunctions::createFolder(const std::string &folder)
+{
+    if (folder.empty())
+    {
         throw std::runtime_error("You set an invalid string for this type of operation or the string you passed as an argument is invalid.");
     }
 
     std::system((MKDIR + folder).c_str());
 }
-void ShellFunctions::removeFolder(const std::string& folder) {
-    if (folder.empty()) {
+void Functions::ShellFunctions::removeFolder(const std::string &folder)
+{
+    if (folder.empty())
+    {
         throw std::runtime_error("You set an invalid string for this type of operation or the string you passed as an argument is invalid.");
     }
 
     std::system((RMDIR + folder).c_str());
 }
+
+// File format namespace
+
+FileFormats::Text::Text(std::string data)
+{
+    try
+    {
+        if (!data.empty())
+        {
+            this->list.push_back(data);
+        }
+    }
+    catch (std::exception err)
+    {
+        throw std::runtime_error("Invalid input for Text() function.");
+    }
+}
+void FileFormats::Text::addContent(std::string &data)
+{
+    try
+    {
+        if (!data.empty())
+        {
+            this->list.push_back(data);
+        }
+    }
+    catch (std::exception err)
+    {
+        throw std::runtime_error("Invalid string for Text() function.");
+    }
+}
+// void FileFormats::Text::deleteFile(std::string &namefile);
+// void FileFormats::Text::createFile(int &file);
